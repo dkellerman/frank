5d8d7205c6677f73d91f623d916cef21c243f435
David Kellerman <david@silvermoon.org>
2025-08-16 12:51:55 -0700

    supabase

--- client/src/supabase.ts
+++ client/src/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log('VITE_SUPABASE_URL', import.meta.env.VITE_SUPABASE_URL);
+
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
